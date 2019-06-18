678b15ceff8cbfc04daab0e20808168fca990ca7
Umisyo <kusunokisouta@gmail.com>
2019-06-18 15:45:02 +0900

    fix:若干の変更

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -6,6 +6,7 @@ def default_message(message):
     message.reply('デフォルトのリプライです')
 
 
-@listen_to('help-calender')
+@listen_to('!help-calender')
 def help_calender(message):
     message.reply('カレンダーのヘルプです')
+    
